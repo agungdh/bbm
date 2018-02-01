c617b04a4d1431927cda84200355f0af10b6fd82
agungdh <agungdh@live.com>
2018-02-01 14:47:58 +0700

    update menu

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -25,10 +25,10 @@
 			<li class="treeview">
 				<a href="#"><i class="fa fa-database text-aqua"></i> <span>Master Data </span> <i class="fa fa-angle-left pull-right"></i></a>
 		          <ul class="treeview-menu">
-		            <li><a href=""><i class="fa fa-cubes text-aqua"></i> Data Konsumen</a></li>
-		            <li><a href=""><i class="fa fa-cubes text-aqua"></i> Data Kendaraan</a></li>
-		            <li><a href=""><i class="fa fa-cubes text-aqua"></i> Data Produk</a></li>
-		            <li><a href=""><i class="fa fa-cubes text-aqua"></i> Data Sopir</a></li>
+		            <li><a href="<?php echo base_url('konsumen')?>"><i class="fa fa-cubes text-aqua"></i> Data Konsumen</a></li>
+		            <li><a href="<?php echo base_url('kendaraan')?>"><i class="fa fa-cubes text-aqua"></i> Data Kendaraan</a></li>
+		            <li><a href="<?php echo base_url('produk')?>"><i class="fa fa-cubes text-aqua"></i> Data Produk</a></li>
+		            <li><a href="<?php echo base_url('sopir')?>"><i class="fa fa-cubes text-aqua"></i> Data Sopir</a></li>
 		          </ul>
 			</li>		
 
